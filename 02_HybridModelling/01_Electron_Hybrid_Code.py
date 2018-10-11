23167d0154689f6cc38858ec3e647373f17ba7d4
flohold <florian.holderied@tum.de>
2018-10-11 16:07:35 +0200

    some changes

--- 02_HybridModelling/01_Electron_Hybrid_Code.py
+++ 02_HybridModelling/01_Electron_Hybrid_Code.py
@@ -1,6 +1,3 @@
-
-# coding: utf-8
-
 import numpy as np
 import time
 from copy import deepcopy
@@ -9,8 +6,14 @@ import Utilitis_HybridCode as utils
 
 
 
-restart = 0                        # ... start the simulation from the beginning (0) or continue (1)
-title = 'Results/01_NoDipoleField/02_ZeroIC/simulation_data_T=1000_L=80.txt'   # ... directory for saving data
+# ... start the simulation from the beginning (0) or continue (1)
+restart = 0 
+
+# ... directory for saving data
+title = 'Results/01_NoDipoleField/simulation_data_T=200_L=2pi.txt' 
+
+# ... save only every saving_step-th time step
+saving_step = 5
 
 
 # ... physical parameters
@@ -30,10 +33,9 @@ wperp = 0.53*c                     # ... perpendicular thermal velocity of energ
 
 
 
-
 # ... parameters for initial conditions
 k = 2                              # ... wavenumber of initial wave fields
-ini = 6                            # ... initial conditions for wave fields
+ini = 3                            # ... initial conditions for wave fields
 amp = 1e-4                         # ... amplitude of initial wave fields
 eps = 0.0                          # ... amplitude of spatial pertubation of distribution function 
 # ...
@@ -41,9 +43,9 @@ eps = 0.0                          # ... amplitude of spatial pertubation of dis
 
 
 # ... numerical parameters
-Lz = 80                            # ... length of z-domain
+Lz = 2*np.pi/k                     # ... length of z-domain
 Nz = 256                           # ... number of elements z-direction
-T = 1000.0                         # ... simulation time
+T = 200.0                          # ... simulation time
 dt = 0.05                          # ... time step
 p = 3                              # ... degree of B-spline basis
 Lv = 8                             # ... length of v-domain in each direction (vx,vy,vz)
@@ -54,7 +56,7 @@ Np = np.int(1e5)                   # ... number of energetic simulation particle
 
 
 # ... create parameter list
-pa = np.zeros(8*Nz + 1)
+pa = np.zeros(8*Nz + 5)
 
 pa[0]  = eps0
 pa[1]  = mu0
@@ -82,19 +84,17 @@ pa[22] = Nv
 pa[23] = Np 
 
 pa[27] = 1
+pa[30] = saving_step
 # ...
 
 
 
 # ... discretization parameters
 dz = Lz/Nz
-zj = np.linspace(0,Lz,Nz+1)
-
-Nt = np.int(T/dt)
-tn = np.linspace(0,T,Nt+1)
+zj = np.linspace(0, Lz, Nz + 1)
 
 dv = Lv/Nv
-vj = np.linspace(-Lv/2,Lv/2,Nv+1)
+vj = np.linspace(-Lv/2, Lv/2, Nv+1)
 # ...
 
 
@@ -103,21 +103,21 @@ vj = np.linspace(-Lv/2,Lv/2,Nv+1)
 
 
 # ... system matrices for fluid electrons and electromagnetic fields
-A10 = np.array([0,0,0,c**2,0,0])
-A11 = np.array([0,0,-c**2,0,0,0])
-A12 = np.array([0,-1,0,0,0,0])
-A13 = np.array([1,0,0,0,0,0])
-A14 = np.array([0,0,0,0,0,0])
-A15 = np.array([0,0,0,0,0,0])
-A1 = np.array([A10,A11,A12,A13,A14,A15])
+A10 = np.array([0, 0, 0, +c**2, 0, 0])
+A11 = np.array([0, 0, -c**2, 0, 0, 0])
+A12 = np.array([0, -1, 0, 0, 0, 0])
+A13 = np.array([+1, 0, 0, 0, 0, 0])
+A14 = np.array([0, 0, 0, 0, 0, 0])
+A15 = np.array([0, 0, 0, 0, 0, 0])
+A1 = np.array([A10, A11, A12, A13, A14, A15])
 
-A20 = np.array([0,0,0,0,mu0*c**2,0])
-A21 = np.array([0,0,0,0,0,mu0*c**2])
-A22 = np.array([0,0,0,0,0,0])
-A23 = np.array([0,0,0,0,0,0])
-A24 = np.array([-eps0*wpe**2,0,0,0,0,-wce])
-A25 = np.array([0,-eps0*wpe**2,0,0,wce,0])
-A2 = np.array([A20,A21,A22,A23,A24,A25])
+A20 = np.array([0, 0, 0, 0, mu0*c**2, 0])
+A21 = np.array([0, 0, 0, 0, 0, mu0*c**2])
+A22 = np.array([0, 0, 0, 0, 0, 0])
+A23 = np.array([0, 0, 0, 0, 0, 0])
+A24 = np.array([-eps0*wpe**2, 0, 0, 0, 0, -wce])
+A25 = np.array([0, -eps0*wpe**2, 0, 0, +wce, 0])
+A2 = np.array([A20, A21, A22, A23, A24, A25])
 
 s = int(np.sqrt(A1.size))
 
@@ -129,26 +129,26 @@ def B_background_z(z):
 
 
 # ... time integration 
-def update(uj,particles,Ep,Bp,dt):
+def update(uj, particles, Ep, Bp ,dt):
     
     
     # ... save old positions
-    zold = deepcopy(particles[:,0])
+    zold = deepcopy(particles[:, 0])
     # ...
     
     
     # ... update particle velocities from n-1/2 to n+1/2 with fields at time n and positions from n to n+1 with velocities at n+1/2
-    znew,vnew = utils.borisPush(particles, dt, Bp, Ep, qe, me, Lz)
+    znew, vnew = utils.borisPush(particles, dt, Bp, Ep, qe, me, Lz)
     # ...
     
     
     # ... update weights with control variate
-    wnew = w0 - Maxwell(vnew[:,0], vnew[:,1], vnew[:,2])/g0
+    wnew = w0 - Maxwell(vnew[:, 0], vnew[:, 1], vnew[:, 2])/g0
     # ...
     
     
     # ... compute hot electron current densities
-    jhnew = utils.hotCurrent(vnew[:,0:2], 1/2*(znew + zold), wnew, zj, bsp, qe, c)
+    jhnew = utils.hotCurrent(vnew[:, 0:2], 1/2*(znew + zold), wnew, zj, bsp, qe, c)
     # ...
      
     
@@ -159,7 +159,7 @@ def update(uj,particles,Ep,Bp,dt):
     
     
     # ... time integration of E,B,jc from n to n+1 with Crank-Nicolson method (use hot current density at n+1/2) 
-    ujnew = np.dot(LHSinv,np.dot(RHS,uj) + dt*Fh)
+    ujnew = np.dot(LHSinv,np.dot(RHS, uj) + dt*Fh)
     # ...
     
     
@@ -179,22 +179,21 @@ if restart == 0:
 
 
     # ... initial energetic particle distribution function (perturbed anisotropic Maxwellian)
-    def fh0(z,vx,vy,vz,eps):
+    def fh0(z, vx, vy, vz, eps):
         return (1 + eps*np.cos(k*z))*nh/((2*np.pi)**(3/2)*wpar*wperp**2)*np.exp(-vz**2/(2*wpar**2) - (vx**2 + vy**2)/(2*wperp**2))
     # ...
 
 
     # ... Maxwellian for control variate
-    def Maxwell(vx,vy,vz):
+    def Maxwell(vx, vy, vz):
         return nh/((2*np.pi)**(3/2)*wpar*wperp**2)*np.exp(-vz**2/(2*wpar**2) - (vx**2 + vy**2)/(2*wperp**2))
     # ...
 
 
     # ... sampling distribution for initial markers
-    def g_sampling(vx,vy,vz):
+    def g_sampling(vx, vy, vz):
         return 1/((2*np.pi)**(3/2)*wpar*wperp**2)*np.exp(-vz**2/(2*wpar**2) - (vx**2 + vy**2)/(2*wperp**2))*1/Lz
     # ...
-
 
 
 
@@ -210,10 +209,10 @@ if restart == 0:
     uj = np.zeros(s*Nb)               # ... coefficients for Galerkin approximation
     Fh = np.zeros(s*Nb)               # ... RHS of matrix system
 
-    Mblock = np.zeros((s*Nb,s*Nb))    # ... block mass matrix    
-    Cblock = np.zeros((s*Nb,s*Nb))    # ... block convection matrix
+    Mblock = np.zeros((s*Nb, s*Nb))   # ... block mass matrix    
+    Cblock = np.zeros((s*Nb, s*Nb))   # ... block convection matrix
 
-    u0 = np.zeros((Nb,s))             # ... L2-projection of initial conditions
+    u0 = np.zeros((Nb, s))            # ... L2-projection of initial conditions
 
     A1block = block_diag(*([A1]*Nb))  # ... block system matrix A1
     A2block = block_diag(*([A2]*Nb))  # ... block system matrix A2
@@ -239,14 +238,14 @@ if restart == 0:
     # ... assemble u0
     timea = time.time()
 
-    for qu in range (0,s):
+    for qu in range (0, s):
         
         def initial(z):
             return utils.IC(z, ini, amp, k, omega = 0)[qu]
 
         u0[:,qu] = utils.L2proj(bsp, Lz, quad_points, weights, M, initial)
 
-    uj = np.reshape(u0,s*Nb)
+    uj = np.reshape(u0, s*Nb)
 
     timeb = time.time()
     print('time for initial vector assembly: ' + str(timeb - timea))
@@ -263,34 +262,31 @@ if restart == 0:
 
 
     # ... create particles (z,vx,vy,vz,wk) and sample positions and velocities according to sampling distribution
-    particles = np.zeros((Np,5))
-    particles[:,0] = np.random.rand(Np)*Lz
-    particles[:,1] = np.random.randn(Np)*wperp
-    particles[:,2] = np.random.randn(Np)*wperp
-    particles[:,3] = np.random.randn(Np)*wpar
+    particles = np.zeros((Np ,5))
+    particles[:, 0] = np.random.rand(Np)*Lz
+    particles[:, 1] = np.random.randn(Np)*wperp
+    particles[:, 2] = np.random.randn(Np)*wperp
+    particles[:, 3] = np.random.randn(Np)*wpar
     # ...
 
 
 
 
     # ... parameters for control variate
-    g0 = g_sampling(particles[:,1],particles[:,2],particles[:,3])
-    w0 = fh0(particles[:,0],particles[:,1],particles[:,2],particles[:,3],eps)/g_sampling(particles[:,1],particles[:,2],particles[:,3])
+    g0 = g_sampling(particles[:, 1], particles[:, 2], particles[:, 3])
+    w0 = fh0(particles[:, 0], particles[:, 1], particles[:, 2], particles[:, 3], eps)/g_sampling(particles[:, 1], particles[:, 2], particles[:, 3])
     # ...
 
 
 
-
-
-
     # ... initial fields at particle positions
-    Ep = np.zeros((Np,3))
-    Bp = np.zeros((Np,3))
-    Bp[:,2] = B0z
+    Ep = np.zeros((Np, 3))
+    Bp = np.zeros((Np, 3))
+    Bp[:, 2] = B0z
 
     timea = time.time()
 
-    Ep[:,0:2], Bp[:,0:2] = utils.fieldInterpolation(particles[:,0], zj, bsp, uj)
+    Ep[:, 0:2], Bp[:, 0:2] = utils.fieldInterpolation(particles[:, 0], zj, bsp, uj)
 
     timeb = time.time()
     print('time for intial field interpolation: ' + str(timeb - timea))
@@ -300,11 +296,13 @@ if restart == 0:
 
 
 
-    # ... initialize velocities by pushing back by -dt/2 and compute weights
+    # ... initialize velocities by pushing back by -dt/2, compute weights and energy of hot particles
     timea = time.time()
+    
+    energy_jh = me/(2*Np)*np.dot(particles[:, 4], particles[:, 1]**2 + particles[:, 2]**2 + particles[:, 3]**2)
 
-    particles[:,1:4] = utils.borisPush(particles, -dt/2, Bp, Ep, qe, me, Lz)[1]
-    particles[:,4] = w0 - Maxwell(particles[:,1], particles[:,2], particles[:,3])/g0
+    particles[:, 1:4] = utils.borisPush(particles, -dt/2, Bp, Ep, qe, me, Lz)[1]
+    particles[:, 4] = w0 - Maxwell(particles[:, 1], particles[:, 2], particles[:, 3])/g0
 
     timeb = time.time()
     print('time for intial particle push: ' + str(timeb - timea))
@@ -317,8 +315,8 @@ if restart == 0:
     # ... compute matrices for field update
     timea = time.time()
 
-    LHS = Mblock + 1/2*dt*np.dot(Cblock,A1block) + 1/2*dt*np.dot(Mblock,A2block)
-    RHS = Mblock - 1/2*dt*np.dot(Cblock,A1block) - 1/2*dt*np.dot(Mblock,A2block)
+    LHS = Mblock + 1/2*dt*np.dot(Cblock, A1block) + 1/2*dt*np.dot(Mblock, A2block)
+    RHS = Mblock - 1/2*dt*np.dot(Cblock, A1block) - 1/2*dt*np.dot(Mblock, A2block)
     LHSinv = np.linalg.inv(LHS)
 
     timeb = time.time()
@@ -329,35 +327,69 @@ if restart == 0:
 
 
 
-    # ... create data file and save parameters (first row) and initial fields (second row)
-    file = open(title,'ab')
+    # ... create data file and save parameters (first row), initial fields and energies (second row)
+    file = open(title, 'ab')
 
+    np.savetxt(file, np.reshape(pa, (1, 8*Nb + 5)), fmt = '%1.5e')
 
-    np.savetxt(file,np.reshape(pa,(1,8*Nb + 1)),fmt = '%1.6e')
-
-    data = np.append(uj,np.zeros(2*Nb))
-    data = np.append(data,tn[0])
-    np.savetxt(file,np.reshape(data,(1,8*Nb + 1)),fmt = '%1.6e')
+    data = np.append(uj, np.zeros(2*Nb))
+    
+    energy_E  = eps0/2*(np.dot(uj[0::s], np.dot(M, uj[0::s])) + np.dot(uj[1::s], np.dot(M, uj[1::s])))
+    
+    energy_B  = eps0/(2*mu0)*(np.dot(uj[2::s], np.dot(M, uj[2::s])) + np.dot(uj[3::s], np.dot(M, uj[3::s])))
+    
+    energy_jc = 1/(2*eps0*wpe**2)*(np.dot(uj[4::s], np.dot(M, uj[4::s])) + np.dot(uj[5::s], np.dot(M, uj[5::s])))
+    
+    data = np.append(data, np.array([energy_E, energy_B, energy_jc, energy_jh]))
+    
+    data = np.append(data, 0.)
+    np.savetxt(file, np.reshape(data, (1,8*Nb + 5)), fmt = '%1.5e')
     # ...
 
 
 
     # ... time loop
-    print('total number of time steps: ' + str(Nt))
+    print('start time integration!')
+    time_step = 0
 
-    for n in range(0,Nt):
+    while True:
 
-        if n%50 == 0:
-            print('time steps finished: ' + str(n))
+        try:
+            if time_step%50 == 0:
+                print('time steps finished: ' + str(time_step))
 
-        particles[:,0],particles[:,1:4],particles[:,4],jh,uj,Ep[:,0:2],Bp[:,0:2] = update(uj,particles,Ep,Bp,dt)
+            particles[:, 0], particles[:, 1:4], particles[:, 4], jh, uj, Ep[:, 0:2], Bp[:, 0:2] = update(uj, particles, Ep, Bp, dt)
 
-        if n%10 == 0:
-            # ... add data to file
-            data = np.append(uj, jh)
-            data = np.append(data, tn[n + 1])
-            np.savetxt(file, np.reshape(data, (1,8*Nb + 1)), fmt = '%1.4e')
-            # ...
+            if time_step%saving_step == 0:
+                
+                # ... add data to file
+                data = np.append(uj, jh)
+                
+                energy_E  = eps0/2*(np.dot(uj[0::s], np.dot(M, uj[0::s])) + np.dot(uj[1::s], np.dot(M, uj[1::s])))
+    
+                energy_B  = eps0/(2*mu0)*(np.dot(uj[2::s], np.dot(M, uj[2::s])) + np.dot(uj[3::s], np.dot(M, uj[3::s])))
+    
+                energy_jc = 1/(2*eps0*wpe**2)*(np.dot(uj[4::s], np.dot(M, uj[4::s])) + np.dot(uj[5::s], np.dot(M, uj[5::s])))
+    
+                energy_jh = me/(2*Np)*np.dot(particles[:, 4], particles[:, 1]**2 + particles[:, 2]**2 + particles[:, 3]**2)
+                
+                data = np.append(data, np.array([energy_E, energy_B, energy_jc, energy_jh]))
+                
+                data = np.append(data, (time_step + 1)*dt)
+                np.savetxt(file, np.reshape(data, (1, 8*Nb + 5)), fmt = '%1.5e')
+                # ...
+                
+            time_step += 1
+        except KeyboardInterrupt:
+            print('Pausing...  (Hit ENTER to continue, type quit to exit.)')
+            try:
+                response = input()
+                if response == 'quit':
+                    break
+                print('Resuming...')
+            except KeyboardInterrupt:
+                print('Resuming...')
+                continue
     # ...
 
 
@@ -371,28 +403,27 @@ if restart == 0:
 if restart == 1:
     
     # ... open data file that hasn't been finished yet
-    file = open(title,'ab')
+    file = open(title, 'ab')
     # ...
 
 
     # ... time loop
     print('total number of time steps: ' + str(Nt))
 
-    for i in range(n,Nt):
+    for i in range(n, Nt):
 
         if i%50 == 0:
             print('time steps finished: ' + str(i))
 
-        particles[:,0],particles[:,1:4],particles[:,4],jh,uj,Ep[:,0:2],Bp[:,0:2] = update(uj,particles,Ep,Bp,dt)
+        particles[:, 0], particles[:, 1:4], particles[:, 4], jh, uj, Ep[:, 0:2], Bp[:, 0:2] = update(uj, particles, Ep, Bp, dt)
 
         if i%10 == 0:
             # ... add data to file
             data = np.append(uj, jh)
             data = np.append(data, tn[i + 1])
-            np.savetxt(file, np.reshape(data, (1, 8*Nb + 1)), fmt = '%1.4e')
+            np.savetxt(file, np.reshape(data, (1, 8*Nb + 1)), fmt = '%1.5e')
             # ...
     # ...
 
 
     file.close()   
-
