83f3952db6f55f8546797046fdd0f467cdcec720
ratnania <ratnaniahmed@gmail.com>
2018-11-12 11:38:38 +0100

    imprve field interpolation

--- pic/opt_utils_v1.py
+++ pic/opt_utils_v1.py
@@ -20,7 +20,7 @@ def find_tiles(particles_pos, Nz, Lz, tilesize):
 # ...
 
 
-
+# ... NOTE: this function can not be pyccelized for the moment
 def fieldInterpolation_bc_1(particles_pos, knots, p, Nb, Lz, ex, ey, bx, by, values):
     tilesize = p
     d_tiles = find_tiles(particles_pos, Nb, Lz, tilesize)
@@ -31,23 +31,29 @@ def fieldInterpolation_bc_1(particles_pos, knots, p, Nb, Lz, ex, ey, bx, by, val
     # ...
     for ie, ipositions in d_tiles.items():
         positions = deepcopy(particles_pos[ipositions])
-        npart = len(positions)
-        for ilpart in range(0, npart):
-            ipart = ipositions[ilpart]
-            pos = positions[ilpart]
-            span = find_span( knots, p, pos )
-            basis_funs( knots, p, pos, span, values )
-
-            for il in range(0, p + 1):
-
-                i = span - il
-                ii = i%Nb
-                bi = values[p-il]
-
-                Ep[ipart, 0] += ex[ii]*bi
-                Ep[ipart, 1] += ey[ii]*bi
-                Bp[ipart, 0] += bx[ii]*bi
-                Bp[ipart, 1] += by[ii]*bi
+        core_fieldInterpolation_bc_1(positions, ipositions, knots, p, Nb, Lz, ex, ey, bx, by, values, Ep, Bp)
     # ...
 
     return Ep, Bp
+# ...
+
+def core_fieldInterpolation_bc_1(positions, ipositions, knots, p, Nb, Lz, ex, ey, bx,
+                                 by, values, Ep, Bp):
+
+    npart = len(positions)
+    for ilpart in range(0, npart):
+        ipart = ipositions[ilpart]
+        pos = positions[ilpart]
+        span = find_span( knots, p, pos )
+        basis_funs( knots, p, pos, span, values )
+
+        for il in range(0, p + 1):
+
+            i = span - il
+            ii = i%Nb
+            bi = values[p-il]
+
+            Ep[ipart, 0] += ex[ii]*bi
+            Ep[ipart, 1] += ey[ii]*bi
+            Bp[ipart, 0] += bx[ii]*bi
+            Bp[ipart, 1] += by[ii]*bi
