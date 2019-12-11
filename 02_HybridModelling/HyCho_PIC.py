f5947a156479557f0d02b5b6a613d446489d3ae0
flohold <florian.holderied@tum.de>
2019-12-11 10:00:38 +0100

    .

--- 02_HybridModelling/HyCho_PIC.py
+++ 02_HybridModelling/HyCho_PIC.py
@@ -112,6 +112,14 @@ def pusher_reflecting(particles, dt, t0, t1, p0, spans_0, L, delta, ex, ey, bx, 
 
     span_0 = 0
     span_1 = 0
+    
+    E   = zeros(3, dtype=float)
+    B   = zeros(3, dtype=float)
+    
+    e_z = zeros(3, dtype=float)
+    rho = zeros(3, dtype=float)
+    
+    e_z[2] = 1.
     # ...
     
     
@@ -122,26 +130,22 @@ def pusher_reflecting(particles, dt, t0, t1, p0, spans_0, L, delta, ex, ey, bx, 
     
     
     # ... needed for Boris algorithm
-    u   = zeros(3, dtype=float)
-    up  = zeros(3, dtype=float)
-    uxb = zeros(3, dtype=float)
-    tmp = zeros(3, dtype=float)
-    E   = zeros(3, dtype=float)
-    B   = zeros(3, dtype=float)
-    S   = zeros(3, dtype=float)
-    e_z = zeros(3, dtype=float)
-    rho = zeros(3, dtype=float)
-    
-    e_z[2] = 1.
-    
     qprime = -dt/2
+    
+    u_minus    = zeros(3, dtype=float)
+    t          = zeros(3, dtype=float)
+    u_minus_xt = zeros(3, dtype=float)
+    u_prime    = zeros(3, dtype=float)
+    S          = zeros(3, dtype=float)
+    u_prime_xS = zeros(3, dtype=float)
+    u_plus     = zeros(3, dtype=float)
     # ...
     
     np = len(particles[:, 0])
     
     
     #$ omp parallel
-    #$ omp do private ( ip, pos, span_0, l0, r0, v0, span_1, l1, r1, v1, E, B, rho, normB, r, S, u, uxb, up, tmp, il0, jl0, i0, basis0, il1, jl1, i1, basis1 )
+    #$ omp do private ( ip, pos, span_0, l0, r0, v0, span_1, l1, r1, v1, E, B, rho, u_minus, gamma, t, u_minus_xt, u_prime, normB, r, S, u_prime_xS, u_plus, il0, jl0, i0, basis0, il1, jl1, i1, basis1 )
     for ip in range(np):
         # ... field evaluation (wave + background)
         pos    = particles[ip, 0]
@@ -206,16 +210,17 @@ def pusher_reflecting(particles, dt, t0, t1, p0, spans_0, L, delta, ex, ey, bx, 
         
         
         # ... Boris push
-        u = particles[ip, 1:4] + qprime*E[:]
-        gamma = sqrt(1. + particles[ip, 1]**2 + particles[ip, 2]**2 + particles[ip, 3]**2)
-        cross(u, B[:], uxb)
-        uxb[:] = qprime*uxb[:]/gamma
+        u_minus = particles[ip, 1:4] + qprime*E
+        gamma = sqrt(1. + u_minus[0]**2 + u_minus[1]**2 + u_minus[2]**2)
+        t = qprime/gamma*B
+        cross(u_minus, t, u_minus_xt)
+        u_prime = u_minus + u_minus_xt
         normB = B[0]**2 + B[1]**2 + B[2]**2
         r = 1 + (qprime/gamma)**2*normB
-        S[:] = 2*(qprime/gamma)*B[:]/r
-        cross(u + uxb, S, tmp)
-        up = u + tmp
-        particles[ip, 1:4] = up + qprime*E[:]
+        S = 2*(qprime/gamma)*B/r
+        cross(u_prime, S, u_prime_xS)
+        u_plus = u_minus + u_prime_xS
+        particles[ip, 1:4] = u_plus + qprime*E
         gamma = sqrt(1. + particles[ip, 1]**2 + particles[ip, 2]**2 + particles[ip, 3]**2)
         particles[ip, 0] += dt*particles[ip, 3]/gamma
         # ...
