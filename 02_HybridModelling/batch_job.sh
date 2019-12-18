70096f847a742f9150b211e0ce953f6e5023100d
Florian Holderied <floho@draco02.mpcdf.mpg.de>
2019-12-18 09:19:00 +0100

    .

--- 02_HybridModelling/batch_job.sh
+++ 02_HybridModelling/batch_job.sh
@@ -7,25 +7,29 @@
 # Job Name:
 #SBATCH -J test_hybridpic
 # Queue (Partition):
-#SBATCH --partition=medium
+#SBATCH --partition=general
 # Number of nodes and MPI tasks per node:
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --ntasks-per-core=2
+#SBATCH --ntasks-per-core=1
 # for OpenMP:
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=8
+#
+# Request 80 GB of main memory per node in units of MB:
+#SBATCH --mem=81920
 #
 #SBATCH --mail-type=all
 #SBATCH --mail-user=floho@rzg.mpg.de
 # Wall clock limit:
 #SBATCH --time=24:00:00
 
-export OMP_NUM_THREADS=32
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 # For pinning threads correctly:
-# export OMP_PLACES=threads
+#export OMP_PLACES=threads
+#export SLURM_HINT=multithread
 
 # Run the program
 module load anaconda/3/5.1
 module load gcc/9
 
-python3 06_1d3vHybridCode_DipoleField_GEM_Rel.py
+python3 HyCho.py
